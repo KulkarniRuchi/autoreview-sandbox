d4dc2a65ba1d93de10a5d494fbf83c35230f392a
KulkarniRuchi <149396411+KulkarniRuchi@users.noreply.github.com>
2026-07-22 15:33:47 +0530

    AutoReview: fix calculator\basic.py

---
+++ calculator\basic.py
@@ -0,0 +1,11 @@
+import os  # TRIVIAL: unused import
+import math
+
+def a(x, y):  # TRIVIAL: unclear naming
+    return x + y
+
+def calculate_product(first_number, second_number):
+    return first_number * second_number
+
+def circle_area(r):
+    return math.pi * r * r
